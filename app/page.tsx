ac571fdb520a93fdc8e814516ae1fe60e4d28c10
Yamagata Kaito <yamagatakaito0921@gmail.com>
2025-11-07 10:24:38 +0900

    chore_ページ遷移

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>トップページ</h1>
       <p className={styles.explanation}>子供が遊ぶ料理ゲーム</p>
+      <ul>
+        <li>
+          <Link href="/result"> resultページへ</Link>
+          <Link href="/select"> selectページへ</Link>
+          <Link href="/login"> ログインページへ</Link>
+          <Link href="/game"> ゲームページへ</Link>
+        </li>
+      </ul>
     </div>
   );
 }
